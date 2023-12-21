2f5c674744a12f66e41cfbde777b9d6c9d7eca3d
Mariulus Marcos de Almeida <mariulus_kol@hotmail.com>
2023-12-21 20:53:51 -0300

    Desafio concluído

--- desafio-2.js
+++ desafio-2.js
@@ -1,9 +1,30 @@
-let saldoDeVitorias = vencerOuPerder(5, 50)
-let nivel = "Ferro"
-
-console.log("O Heroi tem saldo de " + saldoDeVitorias + " e está no nível de " + nivel)
-
+let saldoVitorias = vencerOuPerder(121, 36)
 function vencerOuPerder(vitorias, derrotas) {
-    let saldo = vitorias - derrotas 
-    return saldo
+    let saldoRankeadas = vitorias - derrotas 
+    return saldoRankeadas
 }
+let nivel = ""
+switch(true) {
+    case saldoVitorias <= 10:
+        nivel = "Ferro"
+        break
+    case saldoVitorias >= 11 && saldoVitorias <= 20:
+        nivel = "Bronze"
+        break
+    case saldoVitorias >= 21 && saldoVitorias <= 50:
+        nivel = "Prata"
+        break
+    case saldoVitorias >= 51 && saldoVitorias <= 80:
+        nivel = "Ouro"
+        break
+    case saldoVitorias >= 81 && saldoVitorias <= 90:
+        nivel = "Diamante"
+        break
+    case saldoVitorias >= 91 && saldoVitorias <= 100:
+        nivel = "Lendário"
+        break
+    case saldoVitorias >= 101:
+        nivel = "Imortal"
+        break
+}
+console.log("O Herói tem saldo de vitória de " + saldoVitorias + " e está no nível " + nivel)
